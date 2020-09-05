700325eeb4b094de6ca6cd19675f250a7c0bc3df
DishantBatraDev <dishant.batra2015@gmail.com>
2020-09-05 23:39:13 +0530

    updated the file

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -1,6 +1,6 @@
-import cors from 'cors';
 import express from 'express';
 import {sequelize} from './sequelize';
+import cors from 'cors';
 
 import {IndexRouter} from './controllers/v0/index.router';
 
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://accaba99d40fe493bbce5778132a9e60-89597326.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
